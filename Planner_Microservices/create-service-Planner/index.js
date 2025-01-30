c1d3ca97a40af087fd3424d58dbc717bd95732d2
Cristina-Colcha <133615797+Cristina-Colcha@users.noreply.github.com>
2025-01-30 12:13:24 -0500

    Update index.js

--- Planner_Microservices/create-service-Planner/index.js
+++ Planner_Microservices/create-service-Planner/index.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3001;  
 
 // Middleware
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 
 // Connecting to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
