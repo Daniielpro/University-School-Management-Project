53471161ab4a46dc352e88c63d66d8ae91d25d20
Cristina-Colcha <criscolcha@gmail.com>
2025-02-02 21:16:11 -0500

    creacion docker-composer.yml

--- BACKEND/Planner_Microservices/create-service-Planner/index.js
+++ BACKEND/Planner_Microservices/create-service-Planner/index.js
@@ -5,12 +5,17 @@ const connectDB = require('./config/db');
 const activityRoutes = require('./routes/activityRoutes');
 const errorHandler = require('./middlewares/errorHandler');
 const setupSwagger = require('./swagger/swagger');
+const path = require('path');
+const cors = require('cors'); // Requiere el paquete cors
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 // Middleware
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
+
+// Habilitar CORS para todas las solicitudes
+app.use(cors()); // Permite solicitudes desde cualquier origen
 
 // Conectar a MongoDB
 connectDB();
@@ -20,6 +25,10 @@ app.use('/api', activityRoutes);
 
 // Configurar Swagger
 setupSwagger(app);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html')); // Ajusta la ruta si es necesario
+});
 
 // Manejo de errores
 app.use(errorHandler);
--- BACKEND/msvc_notas/msvcnotes/obj/Debug/net8.0/msvcnotes.AssemblyInfo.cs
+++ BACKEND/msvc_notas/msvcnotes/obj/Debug/net8.0/msvcnotes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("msvcnotes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e613c63dc1d60e4e8e0a714ede880ae3d5168045")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25780e976ded64f0d501cb577a47e1869124d395")]
 [assembly: System.Reflection.AssemblyProductAttribute("msvcnotes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("msvcnotes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
