7aa924fa1511404bbde88471db6abcb9c0d50be0
Cristina-Colcha <criscolcha@gmail.com>
2025-02-11 20:58:19 -0500

    TERCER INTENTO

--- BACKEND/Calculadora/config/db.js
+++ BACKEND/Calculadora/config/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  try {
+  try { 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true 
