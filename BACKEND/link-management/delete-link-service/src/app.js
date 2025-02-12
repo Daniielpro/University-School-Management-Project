7a5a6dcb565e598a7b419ecab4ee504edbdb701c
Cristina-Colcha <133615797+Cristina-Colcha@users.noreply.github.com>
2025-02-12 12:47:29 -0500

    update lm_srvice

--- BACKEND/link-management/delete-link-service/src/app.js
+++ BACKEND/link-management/delete-link-service/src/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const linkRoutes = require('./routes/link.routes'); 
 
-const app = express();
+const app = express(); 
 app.use(express.json());
 app.use(cors());
  
--- BACKEND/link-management/delete-link-service/src/server.js
+++ BACKEND/link-management/delete-link-service/src/server.js
@@ -3,4 +3,4 @@ const app = require('./app');
 const PORT = process.env.PORT || 8086;
 app.listen(PORT, () => {
     console.log(`Servicio de eliminar corriendo en http://localhost:${PORT}`); 
-});
+}); 
