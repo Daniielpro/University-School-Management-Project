85ee217d7990366247db350929da0c43957a8659
Cristina-Colcha <133615797+Cristina-Colcha@users.noreply.github.com>
2025-02-12 14:22:35 -0500

    Update server.js

--- BACKEND/link-management/frontend/server.js
+++ BACKEND/link-management/frontend/server.js
@@ -3,6 +3,7 @@ const app = express();
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 8087;
+
 app.listen(PORT, () => {
     console.log(`Frontend corriendo en http://localhost:${PORT}`); 
 }); 
