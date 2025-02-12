dff5eaee215842114f86de1337f00c474c457167
Cristina-Colcha <133615797+Cristina-Colcha@users.noreply.github.com>
2025-02-12 12:03:59 -0500

    Update app.js

--- BACKEND/link-management/delete-link-service/src/app.js
+++ BACKEND/link-management/delete-link-service/src/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
-const linkRoutes = require('./routes/link.routes');
+const linkRoutes = require('./routes/link.routes'); 
 
 const app = express();
 app.use(express.json());
