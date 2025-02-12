205a45a280eafb90c33df2deb4c328a0dc7efc79
Cristina-Colcha <criscolcha@gmail.com>
2025-02-11 22:28:53 -0500

    Update server.js

--- BACKEND/link-management/delete-link-service/src/server.js
+++ BACKEND/link-management/delete-link-service/src/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 8086;
 app.listen(PORT, () => {
-    console.log(`Servicio de eliminar corriendo en http://localhost:${PORT}`);
+    console.log(`Servicio de eliminar corriendo en http://localhost:${PORT}`); 
 });
