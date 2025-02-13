23169994829b81670d33a4a5bae86f7440b35c7a
Cristina-Colcha <criscolcha@gmail.com>
2025-02-13 13:48:23 -0500

    Update database.py

--- BACKEND/weekly-planner/services/read_services/app/database.py
+++ BACKEND/weekly-planner/services/read_services/app/database.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 # Cargar variables desde .env
 load_dotenv()
 
-DB_USER = os.getenv('DB_USER')
+DB_USER = os.getenv('DB_USER') 
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
